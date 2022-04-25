4641f25781dcf4c2f696624cbe2fbe1f2907ecfc
Biagio Spada <spa.biagio981@gmail.com>
2022-04-25 16:50:21 +0200

    add icon card-homepage1

--- src/app/shared/card-homepage/card-homepage.component.spec.ts
+++ src/app/shared/card-homepage/card-homepage.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CardHomepageComponent } from './card-homepage.component';
 
 describe('CardHomepageComponent', () => {
--- src/app/shared/card-homepage/card-homepage.component.ts
+++ src/app/shared/card-homepage/card-homepage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'cake-card-homepage',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardHomepageComponent implements OnInit {
 
-  constructor() { }
-
+  constructor() {}
+  
   ngOnInit(): void {
+    
   }
 
+  homepageCard = {
+
+    card1:{
+    title:"3.1 milion",
+    h2Title: "users",
+    pFooter:"in the last 30 days",
+  },
+
+    card2:{     
+      title:"40 milion",
+      h2Title: "traders",
+      pFooter:"made in the last 30 days",
+    },
+
+    card3:{
+      title:"$6.7 bilion",
+      h2Title: "staked",
+      pFooter:"total Value Locked",
+    }
+
+  }
+  
+
 }
+
+
