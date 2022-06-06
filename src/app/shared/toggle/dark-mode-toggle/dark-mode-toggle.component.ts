5fa7a8b0b505307d4b9dcc3520e108d507f230aa
Emanuele Coluccini <emacoluccini@gmail.com>
2022-06-06 22:59:57 +0200

    pancake swap versione 0.5

--- src/app/shared/toggle/dark-mode-toggle/dark-mode-toggle.component.ts
+++ src/app/shared/toggle/dark-mode-toggle/dark-mode-toggle.component.ts
@@ -7,9 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DarkModeToggleComponent implements OnInit {
 
+  addClass = false;
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
+
 
 }
