8a9834587f8e791a094cbe9b8f7d0ae37070305a
Emanuele Coluccini <emacoluccini@gmail.com>
2022-06-08 06:11:17 +0200

    pancake swap versione 0.7

---
+++ src/app/pipes/number-suffix.pipe.ts
@@ -0,0 +1,26 @@
+import { IfStmt } from '@angular/compiler';
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'numberSuffix'
+})
+export class NumberSuffixPipe implements PipeTransform {
+
+  transform(value: string, ...args: unknown[]): any {
+    let newValue = parseInt(value)
+    if (newValue < 1000) {
+      return newValue.toFixed(2)
+    }
+
+    if (newValue < 1000) {
+      return newValue.toFixed(2)
+    }
+
+    if (newValue < 1000) {
+      return newValue.toFixed(2)
+    }
+
+
+  }
+
+}
