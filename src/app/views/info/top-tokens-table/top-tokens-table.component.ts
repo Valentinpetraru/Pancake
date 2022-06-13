71fffa6d18d3983555d290df069f90f795fb299c
Emanuele Coluccini <emacoluccini@gmail.com>
2022-06-13 03:19:00 +0200

    info version 0.10

--- src/app/views/info/top-tokens-table/top-tokens-table.component.ts
+++ src/app/views/info/top-tokens-table/top-tokens-table.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { InfoApiService } from 'src/app/services/info-api.service';
 
 
 @Component({
@@ -10,8 +11,64 @@ export class TopTokensTableComponent implements OnInit {
 
   @Input() titleTable: string = '';
 
-  constructor() { }
+  public allTokensOrigin: any = [];
+  public allTokens: any = [];
 
-  ngOnInit(): void { }
+  public currentPage: number = 1;
+  public totalPages: number = 1;
+  public indexPage: number = 0;
+  public arrowBack: boolean = false;
+  public arrowAhead: boolean = true;
+
+  constructor(private infoApi: InfoApiService) { }
+
+  ngOnInit(): void {
+    this.infoApi.getAllTokensInfo().subscribe(
+      (data: any) => {
+        this.allTokensOrigin = data;
+        this.setPagination()
+      }
+    )
+  }
+
+  /*SET PAGINATION */
+  setPagination(): void {
+
+    this.allTokens = this.allTokensOrigin.slice(0, 10);
+    this.totalPages = Math.round(this.allTokensOrigin.length / 10 + 0.4);
+    if (this.currentPage === this.totalPages) {
+      this.arrowAhead = false;
+    }
+  }
+  /*GO TO NEXT PAGE*/
+  nextPage(): void {
+    if (this.currentPage < this.totalPages) {
+      this.indexPage += this.allTokens.length;
+      this.allTokens = this.allTokensOrigin.slice(this.indexPage, this.indexPage + 10)
+      this.currentPage++;
+      this.arrowBack = true;
+      this.arrowBack = true;
+    }
+
+    if (this.currentPage === this.totalPages) {
+      this.arrowAhead = false;
+    }
+
+  }
+
+  /*GO TO PREVIOUS PAGE*/
+  previousPage(): void {
+    if (this.currentPage != 1) {
+      this.allTokens = this.allTokensOrigin.slice(this.indexPage - 10, this.indexPage)
+      this.indexPage -= 10;
+      this.currentPage--;
+      this.arrowAhead = true;
+    }
+
+    if (this.currentPage === 1) {
+      this.arrowBack = false;
+
+    }
+  }
 
 }
